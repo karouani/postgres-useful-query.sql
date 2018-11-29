d12691b4c899a3f35c26f3bfee3c543eef674bd6
KAROUANI <ykarouani@gmail.com>
2018-11-29 14:52:43 +0000

    Update and rename README.md to README.sql

--- README.sql
+++ README.sql
@@ -2,7 +2,6 @@ Get all table sizes:
 
 (Followed by query to get _schema_s sizes)
 
-<code>
 SELECT
   schema_name,
   relname,
@@ -133,7 +132,7 @@ SELECT d.datname AS Name, pg_catalog.pg_get_userbyid(d.datdba) AS Owner,
   CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
     THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname)) 
     ELSE 'No Access' 
-  END AS SIZE 
+  END AS SIZE
 FROM pg_catalog.pg_database d 
 ORDER BY 
   CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT') 
